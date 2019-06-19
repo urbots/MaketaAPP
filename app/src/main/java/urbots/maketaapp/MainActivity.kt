271adcf747cd2a75dd9f29df02a06b958af46699
d3ltcod <d3ltcod@protonmail.com>
2019-06-19 13:17:57 +0200

    Created main view

--- app/src/main/java/urbots/maketaapp/MainActivity.kt
+++ app/src/main/java/urbots/maketaapp/MainActivity.kt
@@ -2,11 +2,27 @@ package urbots.maketaapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val spinner: Spinner = findViewById(R.id.arduino_spinner)
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.arduino_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            spinner.adapter = adapter
+        }
+
     }
 }
